784f2fb1ddf4d7bd891750a2c7ae5f200de28bb2
Artem Panasyuk <artem.panasyuk@gmail.com>
2012-11-09 14:03:38 +0000

    OperationReporter interface
    git-svn-id: https://gridkit.googlecode.com/svn/grid-lab/trunk/nimble@2282 4e96dbfe-9890-11de-ac5d-73b9d092dc0a

--- nimble-core/src/main/java/org/gridkit/nimble/probe/OperationReporter.java
+++ nimble-core/src/main/java/org/gridkit/nimble/probe/OperationReporter.java
@@ -1,61 +1,10 @@
 package org.gridkit.nimble.probe;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import org.gridkit.nimble.util.Seconds;
-
-/**
- * Not thread safe class for reporting operations statistics
- */
-public class OperationReporter {
-    private final SamplerFactory factory;
-
-    private final Map<String, Long> startTimesMs = new HashMap<String, Long>();
-    private final Map<String, Long> startTimesNs = new HashMap<String, Long>();
-
-    public OperationReporter(SamplerFactory factory) {
-        this.factory = new CachingSamplerFactory(factory);
-    }
-    
-    public void start(String operation) {
-        startTimesMs.put(operation, System.currentTimeMillis());
-        startTimesNs.put(operation, System.nanoTime());
-    }
-
-    public void finish(String operation, double unit) {
-        long finishNs = System.nanoTime();
+// TODO add more methods as needed
+public interface OperationReporter {
+    void start(String operation);
         
-        if (!startTimesMs.containsKey(operation) || !startTimesNs.containsKey(operation)) {
-            throw new IllegalStateException();
-        }
-        
-        long startMs = startTimesMs.get(operation);
-        long startNs = startTimesNs.get(operation);
-        
-        try {
-            factory.getSpanSampler(operation).write(
-                unit, Seconds.fromMillis(startMs), Seconds.fromNanos(finishNs - startNs)
-            );
-        } finally {
-            startTimesMs.remove(operation);
-            startTimesNs.remove(operation);
-        }
-    }
+    void finish(String operation);
     
-    public void finish(String operation) {
-        finish(operation, 1.0);
-    }
-    
-    public void scalar(String key, double value) {
-        factory.getScalarSampler(key).write(value);
-    }
-    
-    public void event(String key, double measure) {
-        factory.getPointSampler(key).write(measure, Seconds.currentTime());
-    }
-    
-    public void event(String key) {
-        event(key, 1.0);
-    }
+    void scalar(String key, double value);
 }
---
+++ nimble-core/src/main/java/org/gridkit/nimble/probe/SamplerOperationReporter.java
@@ -0,0 +1,61 @@
+package org.gridkit.nimble.probe;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.gridkit.nimble.util.Seconds;
+
+/**
+ * Not thread safe class for reporting operations statistics
+ */
+public class SamplerOperationReporter implements OperationReporter {
+    private final SamplerFactory factory;
+
+    private final Map<String, Long> startTimesMs = new HashMap<String, Long>();
+    private final Map<String, Long> startTimesNs = new HashMap<String, Long>();
+
+    public SamplerOperationReporter(SamplerFactory factory) {
+        this.factory = new CachingSamplerFactory(factory);
+    }
+    
+    public void start(String operation) {
+        startTimesMs.put(operation, System.currentTimeMillis());
+        startTimesNs.put(operation, System.nanoTime());
+    }
+
+    public void finish(String operation, double unit) {
+        long finishNs = System.nanoTime();
+        
+        if (!startTimesMs.containsKey(operation) || !startTimesNs.containsKey(operation)) {
+            throw new IllegalStateException();
+        }
+        
+        long startMs = startTimesMs.get(operation);
+        long startNs = startTimesNs.get(operation);
+        
+        try {
+            factory.getSpanSampler(operation).write(
+                unit, Seconds.fromMillis(startMs), Seconds.fromNanos(finishNs - startNs)
+            );
+        } finally {
+            startTimesMs.remove(operation);
+            startTimesNs.remove(operation);
+        }
+    }
+    
+    public void finish(String operation) {
+        finish(operation, 1.0);
+    }
+    
+    public void scalar(String key, double value) {
+        factory.getScalarSampler(key).write(value);
+    }
+    
+    public void event(String key, double measure) {
+        factory.getPointSampler(key).write(measure, Seconds.currentTime());
+    }
+    
+    public void event(String key) {
+        event(key, 1.0);
+    }
+}
